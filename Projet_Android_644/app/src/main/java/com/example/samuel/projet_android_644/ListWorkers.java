0a87a1c5fa179e90c5b0fe1f760810be140f70be
jacques.herren <jacques.herren@students.hevs.ch>
2016-10-26 01:04:34 +0200

    List : worker, companies, activities

---
+++ Projet_Android_644/app/src/main/java/com/example/samuel/projet_android_644/ListWorkers.java
@@ -0,0 +1,19 @@
+package com.example.samuel.projet_android_644;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jacques on 26.10.2016.
+ */
+
+public class ListWorkers extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_workers);
+
+
+
+    }
+}
