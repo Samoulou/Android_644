c55fe98ac200d11ebfb85ad3eb450f05a9f323eb
Samuel <sam.copp8@gmail.com>
2016-10-27 20:15:05 +0200

    divers

---
+++ Projet_Android_644/app/src/main/java/com/example/samuel/projet_android_644/recap_hour.java
@@ -0,0 +1,13 @@
+package com.example.samuel.projet_android_644;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recap_hour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recap_hour);
+    }
+}
